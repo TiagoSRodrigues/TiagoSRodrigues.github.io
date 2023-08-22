617c381e7c514fea8992f5f8600580aaab74fbef
Tiago Silva Rodrigues <tiago4r@gmail.com>
2023-08-22 23:46:55 +0100

    sidebar with bg

--- configs/scripts.js
+++ configs/scripts.js
@@ -507,3 +507,16 @@ document.addEventListener("DOMContentLoaded", function () {
         localStorage.setItem('fontSize', `${newSize}px`);
     }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const sidebarLinks = document.querySelectorAll("#sidebar a");
+    const sidebarCheckbox = document.querySelector(".hamburger-menu input");
+
+    sidebarLinks.forEach(link => {
+        link.addEventListener("click", function () {
+            if (window.innerWidth <= 768) {
+                sidebarCheckbox.checked = false;
+            }
+        });
+    });
+});
